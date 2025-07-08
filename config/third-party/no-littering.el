bb120c8611e933d12b5b582d18351ea8b7bbad9f
AsmArtisan256 <asm.artisan256@gmail.com>
2025-07-08 23:34:45 +0100

    remove custom.el for good

--- config/third-party/no-littering.el
+++ config/third-party/no-littering.el
@@ -4,6 +4,9 @@
   (setq
    auto-save-file-name-transforms
    `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
-  (setq custom-file (no-littering-expand-etc-file-name "custom.el"))
+  ; (setq custom-file (no-littering-expand-etc-file-name "custom.el"))
+  (setq custom-file (or (and (fboundp 'null-device) (null-device))
+                     (if (eq system-type 'windows-nt) "NUL" "/dev/null")))
+  (setq custom-save-custom-dont-save t) ; Never save customizations
   (when (file-exists-p custom-file)
     (load custom-file)))

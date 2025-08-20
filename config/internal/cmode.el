9335d7d314ca032e5ecd4eb80fa4f18838cc408e
AsmArtisan256 <asm.artisan256@gmail.com>
2025-08-20 17:01:45 +0100

    Add missing file

---
+++ config/internal/cmode.el
@@ -0,0 +1,19 @@
+;; enable indent-tabs-mode when using c-mode
+
+(use-package cc-mode
+  :ensure nil
+  :bind (:map c-mode-base-map
+	      ("<f12>" . compile))
+  :config
+
+  (setq c-basic-offset 2)
+  (setq tab-width 2)
+  (setq c-default-style "linux")
+
+  (add-hook 'c-mode-common-hook (
+				 lambda ()
+				 (setq indent-tabs-mode t)
+				 )
+	    )
+
+  )

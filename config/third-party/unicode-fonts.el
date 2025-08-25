08012bec233d6c6dc2f4bce1e9802d9b20280b4f
AsmArtisan256 <asm.artisan256@gmail.com>
2025-08-25 23:42:51 +0100

    Improve (slightly) the fonts. To be tested.

--- config/third-party/unicode-fonts.el
+++ config/third-party/unicode-fonts.el
@@ -11,41 +11,37 @@
   (setq-default text-scale-remap-header-line t)
 
   (setq fontaine-presets
-	'((small
-	   :default-family "Iosevka Comfy Motion"
-	   :default-height 140
-	   :variable-pitch-family "Iosevka Comfy Duo")
-	  (regular) ; like this it uses all the fallback values and is named `regular'
-	  (medium
-	   :default-weight semibold
-	   :default-height 155
-	   :bold-weight extrabold)
-	  (large
-	   :inherit medium
-	   :default-height 170)
-	  (presentation
-	   :default-height 180)
-	  (t
-	   ;; I keep all properties for didactic purposes, but most can be
-	   ;; omitted.  See the fontaine manual for the technicalities:
-	   ;; <https://protesilaos.com/emacs/fontaine>.
-	   :default-family "Iosevka Comfy"
-	   :default-weight semibold
-	   :default-height 155
-	   :fixed-pitch-family nil ; falls back to :default-family
-	   :fixed-pitch-weight nil ; falls back to :default-weight
-	   :fixed-pitch-height 1.0
-	   :fixed-pitch-serif-family nil ; falls back to :default-family
-	   :fixed-pitch-serif-weight nil ; falls back to :default-weight
-	   :fixed-pitch-serif-height 1.0
-	   :variable-pitch-family "Iosevka Comfy Motion Duo"
-	   :variable-pitch-weight nil
-	   :variable-pitch-height 1.0
-	   :bold-family nil ; use whatever the underlying face has
-	   :bold-weight bold
-	   :italic-family nil
-	   :italic-slant italic
-	   :line-spacing nil)))
+	      '((small
+           :default-family "Iosevka Custom"
+           :default-height 140
+           :variable-pitch-family "Iosevka Custom"
+           :default-width condensed)
+          (regular) ; like this it uses all the fallback values and is named `regular'
+          (medium
+           :default-weight semibold
+           :default-height 155
+           :bold-weight extrabold
+           :default-width condensed)
+          (large
+           :inherit medium
+           :default-height 170)
+          (presentation
+           :default-height 180)
+          (t
+           ;; I keep all properties for didactic purposes, but most can be
+           ;; omitted.  See the fontaine manual for the technicalities:
+           ;; <https://protesilaos.com/emacs/fontaine>.
+           :default-family "Iosevka Custom"
+           :default-weight semibold
+           :default-height 155
+           :default-width condensed
+           :variable-pitch-family "Iosevka Custom"
+           :variable-pitch-weight regular
+           :variable-pitch-height 155
+           :variable-pitch-height 1.0
+           )
+          )
+        )
 
   ;; Set last preset or fall back to desired style from `fontaine-presets'.
   (fontaine-set-preset (or (fontaine-restore-latest-preset) 'regular))
@@ -59,6 +55,11 @@
 
   (define-key global-map (kbd "C-c f") #'fontaine-set-preset)
   (define-key global-map (kbd "C-c F") #'fontaine-set-face-font)
+
+  ;; other font settings
+  (set-face-attribute 'font-lock-comment-face nil :slant 'italic)
+  (set-face-attribute 'font-lock-comment-delimiter-face nil :slant 'italic)
+  (set-face-attribute 'font-lock-comment-delimiter-face nil :slant 'italic)
   )
 
 (use-package unicode-fonts

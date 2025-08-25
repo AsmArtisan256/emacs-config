a732f563f10ae5e301d19996a9fea4b4fef933fa
AsmArtisan256 <asm.artisan256@gmail.com>
2025-08-25 23:50:48 +0100

    enable mixed-pitch for org-mode and text-mode

--- config/third-party/mixed-pitch.el
+++ config/third-party/mixed-pitch.el
@@ -3,5 +3,7 @@
 (use-package mixed-pitch
   :defer 1
   :commands mixed-pitch-mode
+  :hook ((text-mode . mixed-pitch-mode)
+         (org-mode  . mixed-pitch-mode))
   :custom
   (mixed-pitch-set-height t))

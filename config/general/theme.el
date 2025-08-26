3616b8511a870d5154fd7172e10b5201e7e42b64
AsmArtisan256 <asm.artisan256@gmail.com>
2025-08-26 21:29:51 +0100

    switch theme to a more performant one // fix icons for server

--- config/general/theme.el
+++ config/general/theme.el
@@ -1,58 +1,58 @@
 ;; -*- lexical-binding: t; -*-
 
-(use-package catppuccin-theme
-  :ensure t
-  :demand t
-  :config
-  (load-theme 'catppuccin :no-confirm)
+;; (use-package catppuccin-theme
+;;   :ensure t
+;;   :demand t
+;;   :config
+;;   (load-theme 'catppuccin :no-confirm)
 
-  (setq catppuccin-flavor 'mocha)
-  (catppuccin-set-color 'base "#11111b")
-  (catppuccin-reload)
+;;   (setq catppuccin-flavor 'mocha)
+;;   (catppuccin-set-color 'base "#11111b")
+;;   (catppuccin-reload)
 
-  (custom-set-faces
-   ;; modeline customizations
-   '(mode-line ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
-   '(mode-line-active ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
-   '(mode-line-inactive ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
-   ;; show paren match customizations
-   '(show-paren-match ((t (:foreground "#f38ba8" :background "#45475a" :underline t :weight bold))))
+;;   (custom-set-faces
+;;    ;; modeline customizations
+;;    '(mode-line ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
+;;    '(mode-line-active ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
+;;    '(mode-line-inactive ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
+;;    ;; show paren match customizations
+;;    '(show-paren-match ((t (:foreground "#f38ba8" :background "#45475a" :underline t :weight bold))))
+;;    )
+;;   )
+
+(use-package modus-themes
+  :custom
+  (modules-themes-custom-auto-reload nil)
+  (modus-themes-mixed-fonts t)
+  (modus-themes-variable-pitch-ui t)
+  (modus-themes-italic-constructs t)
+  (modus-themes-bold-constructs nil)
+  (modus-themes-org-blocks nil)
+  (modus-themes-completions '((t . (extrabold))))
+  (modus-themes-prompts '(extrabold))
+
+  (modus-themes-headings
+   '((agenda-structure . (variable-pitch semibold 2.2))
+     (agenda-date . (variable-pitch semibold 1.3))
+     (t . (semibold 1.15))))
+
+  (modus-themes-with-colors
+   ;; (set-face-attribute 'fill-column-indicator nil
+   ;;			:background bg-inactive
+   ;;			:foreground bg-inactive)
+   (custom-set-faces
+    `(separator-line ((,class :background ,"#242424"))))
+
+   (set-face-attribute 'separator-line nil
+			                 :inherit 'shadow
+			                 :background nil
+			                 :underline t
+			                 :height 1)
+
    )
+
+
+  (modus-themes-common-palette-overrides nil)
+  :config
+  (load-theme 'modus-vivendi :no-confirm)
   )
-
-;; (use-package modus-themes
-;;   :custom
-;;   (modules-themes-custom-auto-reload nil)
-;;   (modus-themes-mixed-fonts t)
-;;   (modus-themes-variable-pitch-ui t)
-;;   (modus-themes-italic-constructs t)
-;;   (modus-themes-bold-constructs nil)
-;;   (modus-themes-org-blocks nil)
-;;   (modus-themes-completions '((t . (extrabold))))
-;;   (modus-themes-prompts '(extrabold))
-
-;;   (modus-themes-headings
-;;    '((agenda-structure . (variable-pitch semibold 2.2))
-;;      (agenda-date . (variable-pitch semibold 1.3))
-;;      (t . (semibold 1.15))))
-
-;;   (modus-themes-with-colors
-;;     ;; (set-face-attribute 'fill-column-indicator nil
-;;     ;;			:background bg-inactive
-;;     ;;			:foreground bg-inactive)
-;;     (custom-set-faces
-;;      `(separator-line ((,class :background ,"#242424"))))
-
-;;     (set-face-attribute 'separator-line nil
-;; 			:inherit 'shadow
-;; 			:background nil
-;; 			:underline t
-;; 			:height 1)
-
-;;     )
-
-
-;; (modus-themes-common-palette-overrides nil)
-;; :config
-;; (load-theme 'modus-vivendi :no-confirm)
-;; )
--- config/third-party/marginalia.el
+++ config/third-party/marginalia.el
@@ -8,10 +8,16 @@
   (marginalia-mode)
   )
 
-(use-package all-the-icons :if (display-graphic-p))
+(use-package all-the-icons)
 (use-package all-the-icons-completion
   :after (marginalia all-the-icons)
   :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
   :init
   (all-the-icons-completion-mode)
+  :config
+  (defun my/enable-all-the-icons-on-frame ()
+    (when (display-graphic-p)
+      (all-the-icons-completion-mode 1)))
+
+  (add-hook 'server-after-make-frame-hook #'my/enable-all-the-icons-on-frame)
   )

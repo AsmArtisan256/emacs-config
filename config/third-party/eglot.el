fcdfec86f068e8f89e45af1bdceab86967776a82
AsmArtisan256 <asm.artisan256@gmail.com>
2025-08-27 13:25:39 +0100

    Improve eglot configuration

--- config/third-party/eglot.el
+++ config/third-party/eglot.el
@@ -6,17 +6,17 @@
   :ensure t
   :defer t
   :bind (("C-; a"   . eglot-code-actions)
-	 ("C-; r n" . eglot-rename)
-	 ("C-c k" . eldoc)
-	 ("C-; e f" . eglot-format)
-	 ("C-; e a" . eglot-code-actions)
-	 ("C-; e e" . flycheck-list-errors)
-	 ("C-; x d" . xref-find-definitions)
-	 ("C-; x r" . xref-find-references)
-	 ("C-; x p" . xref-go-back)
-	 ("C-; x n" . xref-go-forward)
-	 ("C-; x a" . xref-find-apropos-at-point)
-	 )
+	       ("C-; r n" . eglot-rename)
+	       ("C-c k" . eldoc)
+	       ("C-; f" . eglot-format)
+	       ("C-; a" . eglot-code-actions)
+	       ("C-; e" . flycheck-list-errors)
+	       ("C-; d" . xref-find-definitions)
+	       ("C-; r" . xref-find-references)
+	       ("C-; p" . xref-go-back)
+	       ("C-; n" . xref-go-forward)
+	       ("C-; x a" . xref-find-apropos-at-point)
+	       )
   :hook
   (
    ((c-mode c++-mode python-mode zig-mode) . eglot-ensure)
@@ -27,7 +27,7 @@
 
   ;; for non project files
   (eglot-extend-to-xref t)
-  (eglot-auto-reconnect t)
+  (eglot-autoreconnect nil)
   (eglot-connect-timeout nil)
   (eglot-sync-connect nil)
   :config
@@ -35,16 +35,16 @@
   (fset #'jsonrpc--log-event #'ignore)
 
   (setq completion-category-overrides '((eglot (styles orderless))
-					(eglot-capf (styles orderless))))
+					                              (eglot-capf (styles orderless))))
 
   (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster)
 
   (defun my/lsp-mode ()
     ;; dunno whatever this does idc
     (setq-local completion-at-point-functions
-		(list (cape-capf-super
-		       #'eglot-completion-at-point
-		       #'tempel-expand)))
+		            (list (cape-capf-super
+		                   #'eglot-completion-at-point
+		                   #'tempel-expand)))
 
     ;; disable inlay hints
     (eglot-inlay-hints-mode -1)
@@ -57,14 +57,14 @@
 
   ;; server programs
   (add-to-list 'eglot-server-programs
-	       '((c++-mode c-mode cc-mode)
-		 "clangd"
-		 "-j=2"
-		 "--background-index"
-		 "--clang-tidy"
-		 "--completion-style=bundled"
-		 "--header-insertion=never"
-		 "--header-insertion-decorators"))
+	             '((c++-mode c-mode cc-mode)
+		             "clangd"
+		             "-j=2"
+		             "--background-index"
+		             "--clang-tidy"
+		             "--completion-style=bundled"
+		             "--header-insertion=never"
+		             "--header-insertion-decorators"))
   )
 
 ;; for C/C++ definitions

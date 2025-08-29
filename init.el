7a2e4c13d5534c414e470ed5e9470a75a4b92565
AsmArtisan256 <asm.artisan256@gmail.com>
2025-08-29 16:54:48 +0100

    Add idle timer for memory cleanup function

--- init.el
+++ init.el
@@ -63,6 +63,13 @@
   ;; Prevents error if the custom.el file doesn't exist
   (load custom-file 'noerror))
 
+;; gc cleanup
+(defun gc-and-trim ()
+  (interactive)
+  (garbage-collect)
+  (malloc-trim))
+(run-with-idle-timer 10 t 'gc-and-trim)
+
 ;; other straight stuff that needs to be here
 ;; (use-package org :straight (:type built-in))
 

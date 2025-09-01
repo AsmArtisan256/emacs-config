3f5d4c6072520040a70631c215087bacbcebff6c
AsmArtisan256 <asm.artisan256@gmail.com>
2025-09-01 20:24:14 +0100

    Set for the default theme and getting on with it

--- config/general/theme.el
+++ config/general/theme.el
@@ -1,24 +1,24 @@
 ;; -*- lexical-binding: t; -*-
 
-(use-package catppuccin-theme
-  :ensure t
-  :demand t
-  :config
-  (load-theme 'catppuccin :no-confirm)
+;; (use-package catppuccin-theme
+;;   :ensure t
+;;   :demand t
+;;   :config
+;;   (load-theme 'catppuccin :no-confirm)
 
-  (setq catppuccin-flavor 'mocha)
-  (catppuccin-set-color 'base "#11111b")
-  (catppuccin-reload)
+;;   (setq catppuccin-flavor 'mocha)
+;;   (catppuccin-set-color 'base "#11111b")
+;;   (catppuccin-reload)
 
-  (custom-set-faces
-   ;; modeline customizations
-   '(mode-line ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
-   '(mode-line-active ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
-   '(mode-line-inactive ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
-   ;; show paren match customizations
-   '(show-paren-match ((t (:foreground "#f38ba8" :background "#45475a" :underline t :weight bold))))
-   )
-  )
+;;   (custom-set-faces
+;;    ;; modeline customizations
+;;    '(mode-line ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
+;;    '(mode-line-active ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
+;;    '(mode-line-inactive ((t (:box (:line-width 2 :color "#cba6f7") :background "#11111b" :foreground "#cdd6f4" :weight bold))))
+;;    ;; show paren match customizations
+;;    '(show-paren-match ((t (:foreground "#f38ba8" :background "#45475a" :underline t :weight bold))))
+;;    )
+;;   )
 
 ;; (use-package modus-themes
 ;;   :custom
@@ -57,8 +57,15 @@
 ;;   (load-theme 'modus-vivendi :no-confirm)
 ;;   )
 
-;; (use-package tao-theme
-;;   :ensure t
-;;   :demand t
-;;   :config
-;;   (load-theme 'tao-yin :no-confirm))
+;; the definitive theme TBHBBQFAM (the lightest of them all)
+(add-to-list 'default-frame-alist '(reverse . t))
+(setq initial-frame-alist default-frame-alist)
+
+;; line highlight
+(require 'hl-line)
+(set-face-background 'hl-line "#3e4446")
+(set-face-foreground 'highlight nil)
+
+
+;; selection
+(set-face-attribute 'region nil :background "#666")
